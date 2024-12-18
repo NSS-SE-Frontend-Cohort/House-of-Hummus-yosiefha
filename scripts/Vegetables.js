7897e120b034cb005f7a530a1262559cf004bced
yosiefha <joehailem1@gmail.com>
2024-12-18 09:51:15 -0600

    Vegies

--- scripts/Vegetables.js
+++ scripts/Vegetables.js
@@ -1,4 +1,6 @@
-export const Veggies = () => {
+export const getVegetables = async () => {
+    const response = await fetch(`http://localhost:8088/vegetables`);
+    const vegetables = await response.json();
+    return vegetables;
+};
 
-    return html
-}
